f95d360a89baaf1adcb3826b2b1c2a6a56a3cec2
awaghat99 <awaghat99@gmail.com>
2023-11-01 16:41:43 +0000

    add about us section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Splash from "../components/Splash/Splash";
+import About from "../components/About/About";
 
 const Home = () => {
   return (
     <div>
       <Splash />
+      <About />
     </div>
   );
 };

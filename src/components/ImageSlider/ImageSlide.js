d57d92521f8821d68f116dbfe102ac03f7fae8fd
awaghat99 <awaghat99@gmail.com>
2023-11-07 15:45:39 +0000

    finish image slider

--- src/components/ImageSlider/ImageSlide.js
+++ src/components/ImageSlider/ImageSlide.js
@@ -2,16 +2,44 @@ import { useState } from "react";
 import "./ImageSlide.css";
 import arrowLeft from "../../images/arrow-left.svg";
 
-const ImageSlide = ({ slides }) => {
-  const [currentIndex] = useState(0);
+const ImageSlide = (props) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-  console.log(`url(${slides[currentIndex].url})`);
+  const goPreviousSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    isFirstSlide ? setCurrentIndex(props.slides.length - 1) : setCurrentIndex(currentIndex - 1);
+  };
+
+  const goNextSlide = () => {
+    const isLastSlide = currentIndex === props.slides.length - 1;
+    isLastSlide ? setCurrentIndex(0) : setCurrentIndex(currentIndex + 1);
+  };
+
+  const goToSlide = (slideIndex) => {
+    setCurrentIndex(slideIndex);
+  };
 
   return (
     <div className="image-slide-container">
-      <img className="left-arrow" src={arrowLeft} alt="arrow-left" />
-      <img className="right-arrow" src={arrowLeft} alt="arrow-left" />
-      <div className="image-slide" style={{ backgroundImage: `url(${slides[currentIndex].url})` }}></div>
+      <img className="left-arrow" src={arrowLeft} alt="arrow-left" onClick={goPreviousSlide} />
+      <img className="right-arrow" src={arrowLeft} alt="arrow-left" onClick={goNextSlide} />
+      <div
+        className="image-slide"
+        style={{ backgroundImage: `url(${props.slides[currentIndex].url})` }}></div>
+      <div className="dot-holder">
+        {props.slides.map((slide, slideIndex) => {
+          return (
+            <div
+              className="dot"
+              key={slideIndex}
+              onClick={() => {
+                goToSlide(slideIndex);
+              }}>
+              &#8226;
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };

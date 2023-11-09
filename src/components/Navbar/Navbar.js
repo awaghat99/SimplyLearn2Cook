696afe9e4e695a70c69de68aacae5d06db4e63fb
awaghat99 <awaghat99@gmail.com>
2023-11-09 11:29:03 +0000

    navbar complete

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,6 +3,10 @@ import "./Navbar.css";
 import hamburger from "../../images/hamburger.svg";
 import cross from "../../images/cross.svg";
 import cookLogo from "../../images/cook-logo.jpeg";
+import germanyFlag from "../../images/germany-flag.svg";
+import franceFlag from "../../images/france-flag.svg";
+import instagram from "../../images/instagram.svg";
+import cart from "../../images/cart.svg";
 
 const Navbar = () => {
   const [dropdown, setDropdown] = useState(false);
@@ -15,6 +19,11 @@ const Navbar = () => {
     <div className="nav">
       <nav>
         <ul className="full-nav">
+          {dropdown ? (
+            <img className="cross menu-icon" src={cross} alt="cross" onClick={toggleDropdown} />
+          ) : (
+            <img className="hamburger menu-icon" src={hamburger} alt="hamburger" onClick={toggleDropdown} />
+          )}
           <div className="logo">
             <li>
               <img src={cookLogo} alt="logo" />
@@ -22,28 +31,66 @@ const Navbar = () => {
           </div>
           <div className="page-links">
             <li>
-              <a href="#about">About</a>
+              <a href="#about">About Story</a>
             </li>
             <li>
-              <a href="#projects">Products</a>
+              <a href="#projects">FlavaExplorer</a>
             </li>
-            <button>
-              <a href="#contact">Contact</a>
-            </button>
-
-            {dropdown ? (
-              <img src={cross} alt="cross" onClick={toggleDropdown} />
-            ) : (
-              <img src={hamburger} alt="hamburger" onClick={toggleDropdown} />
-            )}
+            <li>
+              <a href="#projects">Recipes</a>
+            </li>
+            <li>
+              <a href="#projects">Our Community</a>
+            </li>
+            <li>
+              <a href="#projects">Events</a>
+            </li>
+            <li>
+              <a href="#projects">Blog</a>
+            </li>
+          </div>
+          <div className="flags-and-socials">
+            <li>
+              <span>
+                <img src={franceFlag} alt="france-flag" />
+              </span>
+            </li>
+            <li>
+              <span>
+                <img src={germanyFlag} alt="germany-flag" />
+              </span>
+            </li>
+            <li>
+              <span>
+                <img src={instagram} alt="instagram-logo" />
+              </span>
+            </li>
+            <li>
+              <span>
+                <img src={cart} alt="shopping-cart" />
+              </span>
+            </li>
           </div>
         </ul>
       </nav>
       <ul className={dropdown ? "dropdown open" : "dropdown"}>
-        <li>About</li>
-        <li>Products</li>
         <li>
-          <button>Contact</button>
+          <a href="#about">About Story</a>
+        </li>
+        <li>
+          <a href="#projects">FlavaExplorer</a>
+        </li>
+        <li>
+          <a href="#projects">Recipes</a>
+        </li>
+        <li>
+          <a href="#projects">Our Community</a>
+        </li>
+        <li>
+          <a href="#projects">Events</a>
+        </li>
+        <li>
+          <a href="#projects">Blog</a>
         </li>
       </ul>
     </div>

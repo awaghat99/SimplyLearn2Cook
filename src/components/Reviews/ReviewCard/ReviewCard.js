3bae3e60579942c1d87dbfec7ba2568a835302a2
awaghat99 <awaghat99@gmail.com>
2023-11-10 12:46:28 +0000

    review mobile respnsive

--- src/components/Reviews/ReviewCard/ReviewCard.js
+++ src/components/Reviews/ReviewCard/ReviewCard.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ReviewCard.css";
 import Star from "../Star/Star";
 import placeholder from "../../../images/cook-about-image.jpg";
 
 const ReviewCard = (props) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleReadMore = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
     <div className="review-card">
       <div>
@@ -12,8 +18,13 @@ const ReviewCard = (props) => {
       <h2>{props.heading}</h2>
       <div className="img-and-review">
         <img src={placeholder} alt="placeholder" />
-        <div className="review-text">
+        <div className={`review-text ${isExpanded ? "expanded" : ""}`}>
           <p>{props.review}</p>
+          {props.review.length > 175 && (
+            <span className="read-more" onClick={toggleReadMore}>
+              {isExpanded ? "Read less" : "Read more"}
+            </span>
+          )}
           <p>{props.reviewer}</p>
         </div>
       </div>
